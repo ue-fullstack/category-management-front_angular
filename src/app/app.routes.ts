0eafc6bdf36b4bdb13cf7c08e3e76287d4255f8f
djabidja <djabia-venance.djabia@univ-rouen.fr>
2024-10-24 13:49:30 +0200

    mise ne place menu

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,30 @@
 import { Routes } from '@angular/router';
+import { IndexComponent } from './pages/index/index.component';
+import { LayoutComponent } from './pages/layout/layout.component';
+import { CategoryComponent } from './pages/category/category.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '', //si dans l'url, nous n'avons rien de specific, on le redirige vers login
+        redirectTo:'index',
+        pathMatch:'full'
+    },
+    {
+        path:'index',
+        component:IndexComponent
+    },
+    {
+        path:'',
+        component:LayoutComponent, //ceci pemettra d'afficher le navbar (menu)
+        children: [
+            {
+                path:'index',
+                component:IndexComponent
+            },
+            {
+                path:'category',
+                component:CategoryComponent
+            }
+        ]
+    }
+];
