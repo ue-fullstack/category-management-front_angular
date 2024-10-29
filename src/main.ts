db7cb821d0e097d8dc5751ec0278cc6d3c7d7ee4
Michel Nassalang <nassalang.michel@gmail.com>
2024-10-29 19:12:37 +0100

    feat(header-footer): ajouter des composants header et footer dans shared

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,91 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 
+
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
+
+declare var jQuery: any;
+(function ($) {
+  "use strict";
+
+  // Spinner
+  var spinner = function () {
+    setTimeout(function () {
+      if ($('#spinner').length > 0) {
+        $('#spinner').removeClass('show');
+      }
+    }, 1);
+  };
+  spinner();
+
+
+  // Initiate the wowjs
+  //new WOW().init();
+
+
+  // Sticky Navbar
+  $(window).scroll(() => {
+    if ($(window).scrollTop() > 300) {
+      $('.sticky-top').css('top', '0px');
+    } else {
+      $('.sticky-top').css('top', '-100px');
+    }
+  });
+
+
+
+  // Back to top button
+  $(window).scroll(() => {
+    if ($(window).scrollTop() > 300) {
+      $('.back-to-top').fadeIn('slow');
+    } else {
+      $('.back-to-top').fadeOut('slow');
+    }
+  });
+
+  $('.back-to-top').click(function () {
+    $('html, body').animate({ scrollTop: 0 }, 1500, 'easeInOutExpo');
+    return false;
+  });
+
+
+  // Header carousel
+  $(".header-carousel").owlCarousel({
+    autoplay: true,
+    smartSpeed: 1500,
+    items: 1,
+    dots: true,
+    loop: true,
+    nav : true,
+    navText : [
+      '<i class="bi bi-chevron-left"></i>',
+      '<i class="bi bi-chevron-right"></i>'
+    ]
+  });
+
+
+  // Testimonials carousel
+  $(".testimonial-carousel").owlCarousel({
+    autoplay: true,
+    smartSpeed: 1000,
+    center: true,
+    margin: 24,
+    dots: true,
+    loop: true,
+    nav : false,
+    responsive: {
+      0:{
+        items:1
+      },
+      768:{
+        items:2
+      },
+      992:{
+        items:3
+      }
+    }
+  });
+
+})(jQuery);
+
