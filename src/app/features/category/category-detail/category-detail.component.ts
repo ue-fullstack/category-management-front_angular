c79b1654977e5972f5c18a83224a3e6fc41bc275
Venance Djabia <82992501+Venance5@users.noreply.github.com>
2024-11-22 18:18:22 +0100

    Update category-detail.component.ts

--- src/app/features/category/category-detail/category-detail.component.ts
+++ src/app/features/category/category-detail/category-detail.component.ts
@@ -1,8 +1,10 @@
+/* eslint-disable */
+
 import { Component, inject } from '@angular/core';
 import { CategoryService } from '../../../services/category.service';
 import { Category } from '../../../shared/Category';
 import { ActivatedRoute } from '@angular/router';
-import {CommonModule, DatePipe, NgOptimizedImage} from '@angular/common';
+import { CommonModule, DatePipe } from '@angular/common';
 import { AlertService } from '../../../services/alert.service';
 
 @Component({
@@ -14,12 +16,12 @@ import { AlertService } from '../../../services/alert.service';
 })
 export class CategoryDetailComponent {
   categoryService = inject(CategoryService);
-  category: Category | null = null;
+  category: Category | null = null; 
 
   activateRoute = inject(ActivatedRoute);
 
   constructor(private alertService: AlertService) {
-
+    
     this.activateRoute.params.subscribe((params: any) => {
       const id = +params['id'];
       if (id) {
@@ -29,14 +31,17 @@ export class CategoryDetailComponent {
   }
 
   getCategoryById(id: number) {
-    this.categoryService.getCategoryById(id).subscribe((res: Category) => {
-      this.category = res;
-      // Assurez-vous que les enfants sont correctement chargés
-      if (this.category.children) {
-        console.log('Nombre d\'enfants:', this.category.children.length);
+    this.categoryService.getCategoryById(id).subscribe({
+      next: (category: Category) => {
+        this.category = category;
+        if (this.category.children) {
+          console.log('Nombre d\'enfants:', this.category.children.length);
+        }
+      },
+      error: (error) => {
+        this.alertService.showError(error);
       }
-    }, error => {
-      this.alertService.showAlert("Erreur lors de la récupération des données de la catégorie.");
     });
   }
+  
 }
